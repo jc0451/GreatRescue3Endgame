8ce31e7a1ea2ab5bbdce432adb3ad0c672e0e429
Alex Bugnar <alexbugnar@users.noreply.github.com>
2019-03-27 14:03:26 +0100

    2

--- Assets/Scripts/Misc/Progress.cs
+++ Assets/Scripts/Misc/Progress.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Progress : MonoBehaviour
 {
+    public GameObject N1;
+    public GameObject N2;
+    public GameObject N3;
+    public GameObject N4;
+    public GameObject N5;
+    public GameObject N6;
+    public GameObject N7;
+    public GameObject N8;
+    private int wavenr;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        wavenr = 1;
+        transform.position = N1.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(BgScroll.MoveBg == true)
+        {
+            wavenr++;
+           
+        }
+        if (wavenr == 2)
+            transform.position = Vector2.MoveTowards(transform.position, N2.transform.position, 30 * Time.deltaTime);
+        if (wavenr == 3)
+            transform.position = Vector2.MoveTowards(transform.position, N3.transform.position, 30 * Time.deltaTime);
+        if (wavenr == 4)
+            transform.position = Vector2.MoveTowards(transform.position, N4.transform.position, 30 * Time.deltaTime);
+        if (wavenr == 5)
+            transform.position = Vector2.MoveTowards(transform.position, N5.transform.position, 30 * Time.deltaTime);
+        if (wavenr == 6)
+            transform.position = Vector2.MoveTowards(transform.position, N6.transform.position, 30 * Time.deltaTime);
+        if (wavenr == 7)
+            transform.position = Vector2.MoveTowards(transform.position, N7.transform.position, 30 * Time.deltaTime);
+        if (wavenr == 8)
+            transform.position = Vector2.MoveTowards(transform.position, N8.transform.position, 30 * Time.deltaTime);
     }
 }
