336503aeb8d2b911970c8be82f2af68ec3eca2e8
HampusPaulsen <47183982+HampusPaulsen@users.noreply.github.com>
2019-03-28 07:03:51 +0100

    final commit

---
+++ Assets/FirstSceneScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstSceneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("f1"))
+        {
+
+            UnityEngine.SceneManagement.SceneManager.LoadScene("0. StartScene");
+        }
+
+
+    }
+}
